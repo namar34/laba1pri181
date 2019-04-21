ef42be831f893c3adb7eae100490383035920fc7
Namar <skidanroma@yandex.ru>
2019-04-22 00:20:19 +0400

    ок

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
  int a, b;
  cout << "Enter A and B: ";
  cin >> a >> b;
-cout << "A + B = " << a + b << '\n'
- << "A - B = " << a - b << '\n'
+cout << "A - B = " << a - b << '\n'
+ << "A + B = " << a + b << '\n'
  << "A * B = " << a * b << '\n';
  }
